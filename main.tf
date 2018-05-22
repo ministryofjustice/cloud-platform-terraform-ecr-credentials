cafd9186bf1f0c58083f004f165e1f504fa76a61
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-05-22 15:25:41 +0100

    changed policy to aws_iam_policy_document

--- main.tf
+++ main.tf
@@ -10,43 +10,44 @@ resource "aws_iam_access_key" "key" {
   user = "${aws_iam_user.user.name}"
 }
 
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:UploadLayerPart",
+      "ecr:ListImages",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetRepositoryPolicy",
+      "ecr:PutImage",
+    ]
+    resources = [
+      "arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/${var.team_name}/${var.repo_name}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
+
 resource "aws_iam_policy" "policy" {
   name        = "${var.team_name}-ecr-read-write"
   path        = "/teams/${var.team_name}/"
+  policy      = "${data.aws_iam_policy_document.policy.json}"
   description = "ECR policy for team ${var.team_name}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeImages",
-                "ecr:DescribeRepositories",
-                "ecr:UploadLayerPart",
-                "ecr:ListImages",
-                "ecr:InitiateLayerUpload",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetRepositoryPolicy",
-                "ecr:PutImage"
-            ],
-            "Resource": "arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/${var.team_name}/${var.repo_name}"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ecr:GetAuthorizationToken",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
 }
 
 resource "aws_iam_user_policy_attachment" "policy-attachment" {
-    user       = "${aws_iam_user.user.name}"
-    policy_arn = "${aws_iam_policy.policy.arn}"
+  user       = "${aws_iam_user.user.name}"
+  policy_arn = "${aws_iam_policy.policy.arn}"
 }
