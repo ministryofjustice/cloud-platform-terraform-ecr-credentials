2ea7a724b38f7983309a391ac6daf347ea39a077
Razvan Cosma <36888774+razvan-moj@users.noreply.github.com>
2018-11-23 13:06:39 +0000

    feedback mitch

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "policy" {
     ]
 
     resources = [
-      "arn:aws:ecr:*:*:repository/${var.team_name}/*",
+      "arn:aws:ecr:eu-west-1:754256621582:repository/${var.team_name}/*",
     ]
   }
 
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "policy" {
     ]
 
     resources = [
-      "arn:aws:ecr:*:*:repository/${var.team_name}/*",
+      "*",
     ]
   }
 }
