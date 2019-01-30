af1e34fea3078604f2ac561a573b14d991aca865
ollieanwyll <oliver.anwyll@digital.justice.gov.uk>
2019-01-30 17:10:19 +0000

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -4,5 +4,5 @@ variable "team_name" {}
 
 variable "enable_policy" {
   description = "Sets a ECR lifecycle policy to delete every image after count 900. Default is true."
-  default = true
+  default     = true
 }
