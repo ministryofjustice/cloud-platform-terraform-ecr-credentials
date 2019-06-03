075164fe5cea73e7d26a3c5a756fce753829dba2
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-06-03 11:41:00 +0100

    Remove ecr-lifecycle-policy from the module

--- variables.tf
+++ variables.tf
@@ -2,11 +2,6 @@ variable "repo_name" {}
 
 variable "team_name" {}
 
-variable "enable_policy" {
-  description = "Sets a ECR lifecycle policy to delete every image after count 100. Default is true."
-  default     = true
-}
-
 variable "aws_region" {
   description = "Region into which the resource will be created."
   default     = "eu-west-2"
