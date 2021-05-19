1c3515580c5902c2f7bdf42876a6957599af726e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-05-19 10:18:07 +0100

    localhost

--- test/simple_ecr_repo_test.go
+++ test/simple_ecr_repo_test.go
@@ -21,6 +21,6 @@ func TestSimpleEcrRepo(t *testing.T) {
 	repoArn := terraform.Output(t, terraformOptions, "repo_arn")
 	repoUrl := terraform.Output(t, terraformOptions, "repo_url")
 
-	assert.Equal(t, "arn:aws:ecr:eu-west-2:418216798584:repository/cloud-platform/ecr-repo-unit-test", repoArn)
-	assert.Equal(t, "418216798584.dkr.ecr.eu-west-2.amazonaws.com/cloud-platform/ecr-repo-unit-test", repoUrl)
+	assert.Equal(t, "arn:aws:ecr:eu-west-2:000000000000:repository/cloud-platform/ecr-repo-unit-test", repoArn)
+	assert.Equal(t, "localhost:4510/cloud-platform/ecr-repo-unit-test", repoUrl)
 }
