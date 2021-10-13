b3e8c705c7216fcc337f0c33b8fda1da5ab3ee51
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-10-13 09:26:02 +0100

    Fix tf resources creation.

--- examples/ecr.tf
+++ examples/ecr.tf
@@ -4,10 +4,11 @@
  * releases page of this repository.
  *
  */
-module "example_team_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.3"
-  repo_name = "example-module"
-  team_name = "example-team"
+module "ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.5"
+  team_name = var.team_name
+  repo_name = "${var.namespace}-ecr"
+
   /*
     By default scan_on_push is set to true. When this is enabled then all images pushed to the repo are scanned for any security
     / software vulnerabilities in your image and the results can be viewed in the console. For further details, please see:
@@ -22,17 +23,17 @@ module "example_team_ecr_credentials" {
   # github_repositories = ["my-repo"]
 }
 
-resource "kubernetes_secret" "example_team_ecr_credentials" {
+resource "kubernetes_secret" "ecr_credentials" {
   metadata {
-    name      = "example-team-ecr-credentials-output"
-    namespace = "<NAMESPACE>"
+    name      = "ecr-credentials-output"
+    namespace = var.namespace
   }
 
   data = {
-    access_key_id     = module.example_team_ecr_credentials.access_key_id
-    secret_access_key = module.example_team_ecr_credentials.secret_access_key
-    repo_arn          = module.example_team_ecr_credentials.repo_arn
-    repo_url          = module.example_team_ecr_credentials.repo_url
+    access_key_id     = module.ecr_credentials.access_key_id
+    secret_access_key = module.ecr_credentials.secret_access_key
+    repo_arn          = module.ecr_credentials.repo_arn
+    repo_url          = module.ecr_credentials.repo_url
   }
 }
 
@@ -83,9 +84,8 @@ module "ecr_scan_lambda" {
   # Lambda policy name as desired but unique ideally prefixed with team name e.g webops_ecr_scan_policy
   lambda_policy_name         = "example-team-policy-name"
   slack_secret               = "<SLACK_SECRET_NAME>"
-  namespace                  = "<NAMESPACE>"
+  namespace                  = var.namespace
 
 }
 
 */
-
