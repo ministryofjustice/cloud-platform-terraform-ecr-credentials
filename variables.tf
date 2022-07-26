18213afb9ae91b0e621caaf5ac978b32f7381c32
jakemulley <jakemulley@users.noreply.github.com>
2022-07-26 23:05:21 +0100

    Add missing variable types and descriptions; give providers minimum versions

--- variables.tf
+++ variables.tf
@@ -1,39 +1,45 @@
 variable "repo_name" {
+  description = "Name of the repository to be created"
+  type        = string
 }
 
 variable "team_name" {
-}
-
-variable "aws_region" {
-  description = "Region into which the resource will be created."
-  default     = "eu-west-2"
+  description = "Name of the team creating the credentials"
+  type        = string
 }
 
 variable "scan_on_push" {
-  default = true
+  default     = true
+  description = "Whether images are scanned after being pushed to the repository (true) or not (false)"
+  type        = bool
 }
 
 variable "github_repositories" {
   description = "GitHub repositories in which to create github actions secrets"
   default     = []
+  type        = list(string)
 }
 
 variable "github_actions_secret_ecr_name" {
   description = "The name of the github actions secret containing the ECR name"
   default     = "ECR_NAME"
+  type        = string
 }
 
 variable "github_actions_secret_ecr_url" {
   description = "The name of the github actions secret containing the ECR URL"
   default     = "ECR_URL"
+  type        = string
 }
 
 variable "github_actions_secret_ecr_access_key" {
   description = "The name of the github actions secret containing the ECR AWS access key"
   default     = "ECR_AWS_ACCESS_KEY_ID"
+  type        = string
 }
 
 variable "github_actions_secret_ecr_secret_key" {
   description = "The name of the github actions secret containing the ECR AWS secret key"
   default     = "ECR_AWS_SECRET_ACCESS_KEY"
+  type        = string
 }
--- versions.tf
+++ versions.tf
@@ -1,15 +1,17 @@
 terraform {
+  required_version = ">= 0.14"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
     }
     github = {
-      source = "integrations/github"
-      version = "~> 4.14.0"
+      source  = "integrations/github"
+      version = ">= 4.14.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
     }
   }
-  required_version = ">= 0.14"
 }
