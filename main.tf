484d97a4c659ab4c5242be93ba24093dbae69db9
sj-williams <steve.williams@digital.justice.gov.uk>
2023-06-23 16:38:06 +0100

    force-delete correctly placed

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ resource "aws_ecr_repository" "repo" {
   name = "${var.team_name}/${var.repo_name}"
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
-    force_delete = var.ready_for_deletion
   }
+  force_delete = var.ready_for_deletion
 }
 
 # ECR lifecycle policy
